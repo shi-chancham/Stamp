6f8507519c9280323d05c74fbe389ec37594173d
高橋詩穂 <Shiho@takahashiutaho-no-macbook-pro.local>
2015-12-05 15:23:31 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by 高橋詩穂 on 2015/12/05.
+//  Copyright © 2015年 Shiho Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
